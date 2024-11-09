eaa42ca00ca2ffc3ceddc92b323a41a5427cd32c
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-11-09 17:23:48 +0530

    update some code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Swapnilgupta8585/blog_aggregator/internal/config"
 )
@@ -12,14 +13,28 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = cfg.SetUser("Swapnil")
-	if err != nil {
-		log.Fatal(err)
+	st := &state{
+		config: cfg,
 	}
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatal(err)
+	commandsInstance := &commands{
+		mapToHandlers: make(map[string]func(*state, command) error),
 	}
-	fmt.Println(cfg.DbURL)
-	fmt.Println(cfg.CurrentUserName)
+	commandsInstance.register("login", handlerLogin)
+	argument := os.Args
+	if len(argument) < 2 {
+		fmt.Printf("not enough argumnets provided\n")
+		os.Exit(1)
+	}
+
+	commandInstance := &command{
+		name: argument[1],
+		args: argument[2:],
+	}
+
+	err = commandsInstance.run(st, *commandInstance)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
 }
