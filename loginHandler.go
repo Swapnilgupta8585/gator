a89248aa8c960bba4a8e1e495c49f079bd60a805
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-11-15 17:11:10 +0530

    write logic to find whether user exist or not

--- loginHandler.go
+++ loginHandler.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -9,11 +11,18 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("the login handler expects a single argument, the username")
 	}
 
-	s.config.CurrentUserName = cmd.args[0]
-	err := s.config.SetUser(cmd.args[0])
+	name := cmd.args[0]
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, name)
+	if err != nil {
+		fmt.Printf("%s doesn't exist\n", name)
+		os.Exit(1)
+	}
+	err = s.config.SetUser(name)
 	if err != nil {
 		return err
 	}
+	s.config.CurrentUserName = name
 	fmt.Printf("%s has been set as the Current User\n", cmd.args[0])
 	return nil
 }
