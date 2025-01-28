21999705f98d19bc8a9a2e883ee2babc5591ee03
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-01-29 04:36:46 +0530

    update comments

--- loginHandler.go
+++ loginHandler.go
@@ -8,12 +8,12 @@ import (
 
 // login handler function
 func handlerLogin(st *state, cmd command) error {
-	// if length of cmd.Args method if zero return error
+	// Return an error if no arguments are provided
 	if len(cmd.Args) == 0 {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 
-	// Get the Argument for the login function (name of the user)
+	// Get the username from the command arguments
 	name := cmd.Args[0]
 	ctx := context.Background()
 	_, err := st.db.GetUser(ctx, name)
@@ -22,13 +22,14 @@ func handlerLogin(st *state, cmd command) error {
 		os.Exit(1)
 	}
 
-	// if user exists in Database, Set the user in the config file 
+	// If user exists in the database, update the config file
 	err = st.config.SetUser(name)
 	if err != nil {
 		return err
 	}
-	// Set the user in the Config struct in the state struct
+	// Update the current user in the state config
 	st.config.CurrentUserName = name
-	fmt.Printf("%s has been set as the Current User\n", cmd.Args[0])
+	fmt.Printf("%s has been set as the current user\n", name)
 	return nil
 }
+
