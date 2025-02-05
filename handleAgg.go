273d13f50bad7f07cab0aaefd6626eabba3079a2
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-02-05 07:53:06 +0530

    add comments

--- handleAgg.go
+++ handleAgg.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 )
 
-
-
-
-func handleAgg(st *state, cmd command) error{
+// handleAgg processes the "agg" command by fetching and displaying a feed.
+func handleAgg(st *state, cmd command) error {
+	// Fetch the RSS feed from the given URL.
 	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil{
+	if err != nil {
 		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
-	// +%v adds struct field names to the output as well with thier values
-	fmt.Printf("Feed: %+v\n",feed)
+
+	// %+v formats the struct with field names and values.
+	fmt.Printf("Feed: %+v\n", feed)
 	return nil
 }
