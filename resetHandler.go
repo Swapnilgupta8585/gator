2f3f5b3376fab1dd6f190f891097d4d717f1979e
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-02-05 07:46:09 +0530

    add comments

--- resetHandler.go
+++ resetHandler.go
@@ -6,19 +6,25 @@ import (
 	"os"
 )
 
+// handlerReset processes the reset command by deleting all users and resetting the config.
 func handlerReset(st *state, cmd command) error {
+	// Create a new context.
 	ctx := context.Background()
-	err := st.db.DeleteAllUsers(ctx)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-	err = st.cfg.SetUser("")
-	if err != nil {
+
+	// Delete all users from the database.
+	if err := st.db.DeleteAllUsers(ctx); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Print("reset successful")
+	// Clear the current user from the config file.
+	if err := st.cfg.SetUser(""); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Confirm reset completion.
+	fmt.Println("Reset successful.")
 	return nil
 }
+
