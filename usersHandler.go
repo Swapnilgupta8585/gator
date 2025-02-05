24e6cce3d6ed5fa9813c82e031aba3ecd4ac3723
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-02-05 07:49:37 +0530

    add comments

--- usersHandler.go
+++ usersHandler.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 )
 
+// handlerUsers processes the "users" command by listing all users.
 func handlerUsers(st *state, cmd command) error {
+	// Create a new context.
 	ctx := context.Background()
+
+	// Retrieve all users from the database.
 	userNames, err := st.db.GetUsers(ctx)
 	if err != nil {
 		return err
 	}
+
+	// Display the list of users, marking the current user.
 	for _, val := range userNames {
 		if st.cfg.CurrentUserName == val {
 			fmt.Printf("* %s (current)\n", val)
@@ -18,5 +24,6 @@ func handlerUsers(st *state, cmd command) error {
 		}
 		fmt.Printf("* %s\n", val)
 	}
+
 	return nil
 }
