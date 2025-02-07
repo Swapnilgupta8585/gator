ec1ad532a31490724376f1a9c58bc067a1ec000c
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2025-02-07 11:42:04 +0530

    write the middlware for login verifications

---
+++ middlewareLoggedIn.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/Swapnilgupta8585/blog_aggregator/internal/database"
+)
+
+// middlewareLoggedIn is a middleware that ensures a user is logged in before executing a handler function.
+// It abstracts away repetitive login checks and passes the username to the handler.
+func middlewareLoggedIn(handler func(st *state, cmd command, user database.User) error) func(st *state, cmd command) error {
+	return func (st *state, cmd command) error {
+			user, err := st.db.GetUser(context.Background(), st.cfg.CurrentUserName)
+			if err != nil {
+						return err
+			}
+
+			return handler(st, cmd, user)
+	}
+}
+
+
+	
+
+
